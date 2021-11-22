9eef53c8ad1c2c1ef8afca6f8c36e3cc705e2a79
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-11-22 17:29:28 -0500

    add more onInvalidated tests

--- cypress/integration/invalidate.js
+++ cypress/integration/invalidate.js
@@ -3,6 +3,10 @@
 import '../../src'
 
 describe('Data C', () => {
+  beforeEach(() => {
+    Cypress.clearDataSession('C')
+  })
+
   it('exists under an alias', function () {
     // force invalidation by putting some random data
     Cypress.setDataSession('C', 42)
@@ -20,5 +24,53 @@ describe('Data C', () => {
     cy.get('@C').should('equal', 'c')
     // our initial wrong value was passed to onInvalidate argument
     cy.get('@invalidated').should('be.calledOnceWith', 42)
+  })
+
+  it('calls expected functions (valid)', function () {
+    Cypress.setDataSession('C', 42)
+
+    const setup = cy.stub().as('setup').returns('c')
+    const validate = cy.stub().as('validate').returns(true)
+    const invalidated = cy.stub().as('invalidated')
+
+    cy.dataSession({
+      name: 'C',
+      setup,
+      validate,
+      onInvalidated: invalidated,
+    })
+      .should('equal', 42)
+      .then(() => {
+        expect(validate).to.be.calledOnceWithExactly(42)
+        expect(setup).to.not.be.called
+        expect(invalidated).to.not.be.called
+      })
+  })
+
+  it('calls expected functions (invalidated)', function () {
+    Cypress.setDataSession('C', 42)
+
+    const setup = cy.stub().as('setup').returns('c')
+    const validate = cy.stub().as('validate').returns(false)
+    const invalidated = cy.stub().as('invalidated')
+
+    cy.dataSession({
+      name: 'C',
+      setup,
+      validate,
+      onInvalidated: invalidated,
+    })
+      // changed the data to whatever "setup" yields
+      .should('equal', 'c')
+      .then(() => {
+        expect(validate).to.be.calledOnceWithExactly(42)
+        expect(setup).to.be.calledOnce
+        expect(invalidated).to.be.calledOnceWithExactly(42)
+        // check the order of calls
+        expect(validate, 'validate -> invalidated').to.be.calledBefore(
+          invalidated,
+        )
+        expect(invalidated, 'invalidated -> setup').to.be.calledBefore(setup)
+      })
   })
 })
