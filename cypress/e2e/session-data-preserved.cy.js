97777c8dff69ec751026a22711b4c65ee81068fd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-08-23 18:29:13 -0400

    update some specs

--- cypress/e2e/session-data-preserved.cy.js
+++ cypress/e2e/session-data-preserved.cy.js
@@ -5,6 +5,13 @@ import '../../src'
 // no matter how you reload this test
 // (using the "Run all tests" button or via the browser reload)
 // it preserves the data session
+before(() => {
+  const sessions = Cypress.dataSessions()
+  if (sessions && sessions.length > 0) {
+    Cypress.clearDataSessions()
+  }
+})
+
 describe('Data session is preserved between the tests', () => {
   it('sets data session in the first test', function () {
     cy.dataSession({
