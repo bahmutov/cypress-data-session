07c06bfe0998cb554ec809b5cc714a5d0378111a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-06 19:53:30 -0500

    add comments about validating data session when fetching it

--- cypress/e2e/get-session.cy.js
+++ cypress/e2e/get-session.cy.js
@@ -3,8 +3,11 @@
 import '../../src'
 
 describe('get data', () => {
+  let name
+
   it('creates data session', () => {
     const projectName = `my random project ${Cypress._.random(1e4)}`
+
     // this test prints a new random name,
     // but the data session is the same
     // after the first run
@@ -15,10 +18,15 @@ describe('get data', () => {
         return projectName
       },
       shareAcrossSpecs: true,
-    }).then(cy.log)
+    })
+      .then((s) => (name = s))
+      .then(cy.log)
   })
 
   it('get the data session', () => {
-    cy.dataSession('project name').then(cy.log)
+    cy.dataSession('project name').should('equal', name).then(cy.log)
   })
+
+  // TODO: calls the validate and setup functions
+  // https://github.com/bahmutov/cypress-data-session/issues/93
 })
