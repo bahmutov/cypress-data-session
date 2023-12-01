db50f6c50eea5b299d5dc2f6c701c1ade7fa8c8f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-11-30 20:30:30 -0500

    add another test

--- cypress/e2e/recompute-on-retry.cy.js
+++ cypress/e2e/recompute-on-retry.cy.js
@@ -1,7 +1,7 @@
 // @ts-check
 import '../../src'
 
-describe('when test retries', () => {
+describe('recomputeOnRetry: true', () => {
   let setupCounter
 
   beforeEach(() => {
@@ -11,21 +11,54 @@ describe('when test retries', () => {
     }
   })
 
-  it('recomputes the data session', { retries: 2 }, () => {
+  it(
+    'recomputes the data session when the test retries',
+    { retries: 2 },
+    () => {
+      cy.dataSession({
+        name: 'example',
+        setup() {
+          setupCounter += 1
+          return 'data'
+        },
+        recomputeOnRetry: true,
+      }).then(() => {
+        if (Cypress.currentRetry < 2) {
+          throw new Error(`Retry ${Cypress.currentRetry} error`)
+        }
+
+        // each retry should call the data setup
+        expect(setupCounter, 'data session setup').to.equal(3)
+      })
+    },
+  )
+})
+
+describe('recomputeOnRetry: false (default)', () => {
+  let setupCounter
+
+  beforeEach(() => {
+    if (Cypress.currentRetry === 0) {
+      setupCounter = 0
+      Cypress.clearDataSession('example')
+    }
+  })
+
+  it('reuses the data session when the test retries', { retries: 2 }, () => {
     cy.dataSession({
       name: 'example',
       setup() {
         setupCounter += 1
         return 'data'
       },
-      recomputeOnRetry: true,
     }).then(() => {
       if (Cypress.currentRetry < 2) {
         throw new Error(`Retry ${Cypress.currentRetry} error`)
       }
 
-      // each retry should call the data setup
-      expect(setupCounter, 'data session setup').to.equal(3)
+      // does not recompute the setup
+      // and simply uses the default data session
+      expect(setupCounter, 'data session setup').to.equal(1)
     })
   })
 })
